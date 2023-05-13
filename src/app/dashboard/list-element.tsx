a1be4db2c0e7f4ec6b2eb423719aac48a6f869d5
42akurz <alexku0609@gmail.com>
2023-05-13 12:04:49 +0200

    list for dashborad is done

---
+++ src/app/dashboard/list-element.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import { useState } from "react";
+
+type ListElementProps = {
+  name: string;
+  lastInteraction: string;
+  profileId: string;
+  onSelect: (item: {
+    name: string;
+    lastInteraction: string;
+    profileId: string;
+  }) => void;
+  isSelected: boolean;
+};
+
+export function ListElement({
+  name,
+  lastInteraction,
+  profileId,
+  onSelect,
+  isSelected,
+}: ListElementProps) {
+
+  return (
+    <tr className="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+      <td className="w-4 px-4 py-3">
+        <div className="flex items-center">
+          <input
+            id={`checkbox-table-search-${profileId}`}
+            type="checkbox"
+            onChange={() => onSelect({ name, lastInteraction, profileId })}
+            checked={isSelected}
+            className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor={`checkbox-table-search-${profileId}`}
+            className="sr-only"
+          >
+            checkbox
+          </label>
+        </div>
+      </td>
+      <th
+        scope="row"
+        className="flex items-center px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        <img
+          src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-front-image.png"
+          alt="iMac Front Image"
+          className="w-auto h-8 mr-3"
+        />
+        {name}
+      </th>
+      <td className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        {lastInteraction}
+      </td>
+    </tr>
+  );
+}
